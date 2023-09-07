23078a55c06f94a83dad74a4dd5a2e39c1f2eb26
Jhessmagno <jessicakaroline1102@gmail.com>
2023-09-07 14:53:59 -0300

    First commit

---
+++ JAVASCRIPT/aula 2 - variaveis/script.js
@@ -0,0 +1,109 @@
+// // tipos primitivos
+
+// // boolean: sao os que recebem verdadeiro ou falso
+// var vOuF = false;
+// console.log(typeof(vOuF));
+
+// // number
+// var numeroQualquer = 1;
+// console.log(typeof(numeroQualquer));
+
+// //string
+
+// var nome = "Jessica";
+// console.log(typeof(nome));
+
+// como declarar
+
+// Var
+// var variavel "Jessica";
+// variavel = "martine";
+// console.log(variavel);
+
+// //let
+
+// /let varivel2 = "Jessica";
+// console.log(variavel2);
+
+// const constante = "Jessica";
+// console.log("Constante");
+
+// var escopoGlobal = "global";
+// console.log(escopoGlobal);
+
+// function escopoLocal() {
+//     let escopoLocalInterno = "local";
+//     console.log(escopoLocalInterno);
+// }
+
+// escopoLocal();
+
+// atribuiçao 
+
+// var atribuiçao = "Jessica";
+
+// //comparacao
+
+// var comparacao = "0" == 0;
+// console.log(comparacao);
+
+// // comaparacao identica
+// var comparacaoIdentica = "0" === 0;
+// console.log(comparacaoIdentica);
+
+// // adicao 
+// var adicao = 1+1;
+// console.log(adicao);
+
+// //subtracao 
+// var subtracao= 2-1 ;
+// console.log (subtracao);
+
+// //multiplicacao
+
+// var multiplicacao = 2*3;
+// console.log(multiplicacao);
+
+// // divisao real
+
+// var divisaoReal = 6 / 2;
+// console.log(divisaoReal);
+
+// divisao inteira para receber o resto da divisao utilizamos "%"
+
+// var divisaoInteira = 5 % 2
+// console.log(divisaoInteira);
+
+//potenciacao aqui utilizamos "**" para ser elevado ou seja 2 elevado a 10
+
+// var potenciacao = 2 ** 10; 
+// console.log(potenciacao);
+
+// // operadores relacionais
+//         // maior que
+//         var maiorQue = 5 > 2;
+//         console.log(maiorQue);
+        
+//         // menor que
+//         var menorQue = 5 < 2;
+//         console.log(menorQue);
+        
+//         // maior ou igual a
+//         var maiorOuIgual = 5 >= 2;
+//         console.log(maiorOuIgual);
+        
+//         // menor ou igual a
+//         var menorOuIgual = 5 <= 2;
+//         console.log(menorOuIgual); */
+
+
+       /*  // operadores lógicos
+        // && - todos os valores devem ser true
+        var e = true && true;
+        console.log(e);
+        // || - somente um valor deve ser true
+        var ou = true || false;
+        console.log(ou);
+        // ! - inverte os valores
+        var nao = !true;
+        console.log(nao); */
---
+++ JAVASCRIPT/aula 5 - funcoes e suas particularidades/script.js
@@ -0,0 +1,31 @@
+// funções - functions
+
+// como declarar
+// deve ser declarada usando a palavra function seguida de "()" e "{}"
+/* function funcao() {
+    console.log('mensagem de uma função');
+}
+
+// chamando a função
+funcao(); */
+
+/* 
+// função com parâmetros
+// definindo parâmetros
+function mensagem(primeiro, segundo) {
+    console.log(primeiro, segundo)
+}
+
+// chamando a função atribuindo valor aos parâmetros
+mensagem('Tudo certo', 'jovem!'); */
+
+// function funcao (){
+//     console.log('tudo certo jovem!');
+// }
+
+// funcao();
+
+function mensagem(primeiro, segundo){
+    console.log(primeiro, segundo)
+}
+mensagem('Tudo certo', 'jovem!');
